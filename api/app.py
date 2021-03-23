39dacdc88db4d437087145ceab6ac214503acecf
SteveLim99 <stevelimzilian@gmail.com>
2021-03-24 05:24:26 +0800

    set up backend proxy

--- api/app.py
+++ api/app.py
@@ -3,6 +3,6 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/test')
 def test():
     return "Hello React"
--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -1,25 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  componentDidMount = async () => {
+    fetch('/test')
+      .then(response => {
+        console.log(response.text())
+      })
+      .catch(error => {
+        console.log(error)
+      })
+  }
+
+  render() {
+    return (
+      <div className="App" >
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
