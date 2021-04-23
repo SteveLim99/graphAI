6371c4119ca075139c02677a87e96056e0bed4f8
SteveLim99 <43622493+SteveLim99@users.noreply.github.com>
2021-04-23 12:17:19 +0800

    refactored login

--- login/app.py
+++ login/app.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, request, make_response, jsonify
 from flask import Flask, flash, redirect, url_for, send_file
 from flask.views import MethodView
-from login import encode, decode
+from login import encode, decode, check_if_exist, verify_token
 from flask_bcrypt import Bcrypt
 from dotenv import dotenv_values
 from datetime import datetime
@@ -10,7 +10,8 @@ import psycopg2
 app = Flask(__name__)
 bcrypt = Bcrypt(app)
 
-config = dotenv_values("../env/jwt_secret.env")
+jwt_config = dotenv_values("./env/jwt_secret.env")
+database_config = dotenv_values("./env/database.env")
 
 
 @app.route('/registerUser',  methods=['POST'])
@@ -22,27 +23,26 @@ def register_user():
     res = None
     try:
         conn = psycopg2.connect(
-            host="postgres",
-            database="dev_postgres",
-            user="postgres",
-            password="postgres"
+            host=database_config["POSTGRES_HOST"],
+            database=database_config["POSTGRES_DB"],
+            user=database_config["POSTGRES_USER"],
+            password=database_config["POSTGRES_PASSWORD"]
         )
         cursor = conn.cursor()
-        statement = "select exists(select 1 from users where uname=%s or email=%s limit 1)"
-        cursor.execute(statement, (uName, email,))
-        does_user_exist = bool(cursor.fetchone()[0])
+        does_user_exist = check_if_exist(cursor, None, uName, email)
 
         if not does_user_exist:
-            pw_hash = bcrypt.generate_password_hash(pw)
+            pw_hash = bcrypt.generate_password_hash(pw).decode()
             dt = datetime.today().strftime('%Y-%m-%d  %H:%M:%S')
             statement = "insert into users(uname, email, pw, register_date) values(%s, %s, %s, TO_TIMESTAMP(%s, 'YYYY-MM-DD HH24:MI:SS')) RETURNING uid"
             cursor.execute(statement, (uName, email, pw_hash, dt,))
             uid = cursor.fetchone()[0]
             conn.commit()
-            token = encode(uid)
+            token = encode(uid, jwt_config["SECRET_KEY"])
             res = {
                 'status': 'success',
-                'message': 'Successfully registered.'
+                'message': 'Successfully registered.',
+                'auth_token': token
             }
 
         else:
@@ -51,7 +51,6 @@ def register_user():
                 'message': 'user name or email taken.'
             }
     except (Exception, psycopg2.DatabaseError) as error:
-        print("error" + str(error))
         if conn:
             conn.rollback()
         res = {
@@ -61,4 +60,107 @@ def register_user():
     finally:
         if conn:
             conn.close()
-        return res
+        return make_response(res)
+
+
+@app.route('/login',  methods=['GET'])
+def login_user():
+    uName = request.args.get("uname")
+    pw = request.args.get("pw")
+    conn = None
+    res = None
+
+    try:
+        conn = psycopg2.connect(
+            host=database_config["POSTGRES_HOST"],
+            database=database_config["POSTGRES_DB"],
+            user=database_config["POSTGRES_USER"],
+            password=database_config["POSTGRES_PASSWORD"]
+        )
+        cursor = conn.cursor()
+        does_user_exist = check_if_exist(cursor, None, uName, None)
+
+        if does_user_exist:
+            statement = "select uid, pw from users where uname=%s"
+            cursor.execute(statement, (uName,))
+            cred = list(cursor.fetchone())
+            uid, db_pw = cred[0], cred[1].encode('utf-8')
+            is_pw_true = bcrypt.check_password_hash(db_pw, pw)
+
+            if is_pw_true:
+                token = encode(uid, jwt_config["SECRET_KEY"])
+                res = {
+                    'status': 'success',
+                    'message': 'Logged in',
+                    'auth_token': token
+                }
+
+            else:
+                res = {
+                    'status': 'fail',
+                    'message': 'Incorrect Password.'
+                }
+        else:
+            res = {
+                'status': 'fail',
+                'message': 'User does not exist.'
+            }
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+        if conn:
+            conn.rollback()
+        res = {
+            'status': 'fail',
+            'message': 'Some error occurred. Please try again.'
+        }
+    finally:
+        if conn:
+            conn.close()
+        return make_response(res)
+
+
+@app.route('/logout',  methods=['POST'])
+def check_user_status():
+    token = request.args.get("token")
+    conn = None
+    res = None
+
+    try:
+        conn = psycopg2.connect(
+            host=database_config["POSTGRES_HOST"],
+            database=database_config["POSTGRES_DB"],
+            user=database_config["POSTGRES_USER"],
+            password=database_config["POSTGRES_PASSWORD"]
+        )
+        cursor = conn.cursor()
+
+        token_verification_res = verify_token(
+            token, cursor, jwt_config["SECRET_KEY"])
+        status, msg, uid = token_verification_res["status"], token_verification_res[
+            "message"], token_verification_res["uid"]
+
+        if status:
+            dt = datetime.today().strftime('%Y-%m-%d  %H:%M:%S')
+            statement = "INSERT INTO expired_tokens(uid, token, expiry_date) VALUES(%s, %s, TO_TIMESTAMP(%s, 'YYYY-MM-DD HH24:MI:SS'))"
+            cursor.execute(statement, (uid, token, dt))
+            conn.commit()
+            res = {
+                'status': 'success',
+                'message': 'Logged Out.'
+            }
+        else:
+            res = {
+                'status': 'fail',
+                'message': msg
+            }
+    except (Exception, psycopg2.DatabaseError) as error:
+        if conn:
+            conn.rollback()
+        res = {
+            'status': 'fail',
+            'message': 'Some error occurred. Please try again.'
+        }
+    finally:
+        if conn:
+            conn.close()
+        return make_response(res)
