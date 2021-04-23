da7f8715ff62c95cab032bab6adba90932298e99
SteveLim99 <43622493+SteveLim99@users.noreply.github.com>
2021-04-23 20:42:13 +0800

    gnn api migration to jwt usage

--- gnn_api/app.py
+++ gnn_api/app.py
@@ -5,6 +5,11 @@ import torch
 import dgl
 import model
 import networkx as nx
+from dotenv import dotenv_values
+from lib.utils.login import verify_token
+
+jwt_config = dotenv_values("./env/jwt_secret.env")
+database_config = dotenv_values("./env/database.env")
 
 app = Flask(__name__)
 GML_MODEL = os.getcwd() + "/gcn_model"
@@ -17,33 +22,78 @@ app.config['DOCUMENTATION_FILE'] = DOCUMENTATION_FILE
 
 @app.route('/gmlUpload', methods=['POST'])
 def upload():
-    gnn_model = model.Classifier(1, 256, 2)
+    conn = None
+    res = {
+        'status': 'fail',
+        'message': ''
+    }
 
-    gnn_model.load_state_dict(torch.load(app.config['GML_MODEL']))
-    input_graph = nx.read_gml(app.config['NETWORK_FILE'])
+    try:
+        conn = psycopg2.connect(
+            host=database_config["POSTGRES_HOST"],
+            database=database_config["POSTGRES_DB"],
+            user=database_config["POSTGRES_USER"],
+            password=database_config["POSTGRES_PASSWORD"]
+        )
+        cursor = conn.cursor()
+        token = request.args.get("token")
+        token_verification = verify_token(
+            token, cursor, jwt_config["SECRET_KEY"])
+        token_status = token_verification["status"]
+        token_msg = token_verification["message"]
 
-    graph = dgl.from_networkx(input_graph)
-    graph = dgl.add_self_loop(graph)
+        if token_status:
+            gnn_model = model.Classifier(1, 256, 2)
 
-    res = gnn_model(graph)
+            gnn_model.load_state_dict(torch.load(app.config['GML_MODEL']))
+            input_graph = nx.read_gml(app.config['NETWORK_FILE'])
 
-    probs = torch.softmax(res, 1)
-    sampled_Y = torch.multinomial(probs, 1)
-    argmax_Y = torch.max(probs, 1)[1].view(-1, 1)
+            graph = dgl.from_networkx(input_graph)
+            graph = dgl.add_self_loop(graph)
 
-    np_probs = probs.detach().numpy()[0]
-    int_res = argmax_Y.numpy()[0][0]
+            res = gnn_model(graph)
 
-    str_res = "BPNM"
-    if int_res == 1:
-        str_res = "Swimlane"
+            probs = torch.softmax(res, 1)
+            sampled_Y = torch.multinomial(probs, 1)
+            argmax_Y = torch.max(probs, 1)[1].view(-1, 1)
 
-    content = ""
-    if str_res == "BPNM":
-        with open(app.config['DOCUMENTATION_FILE'] + "bpnm.txt", "r") as f:
-            content = f.read()
-    else:
-        with open(app.config['DOCUMENTATION_FILE'] + "swimlane.txt", "r") as f:
-            content = f.read()
+            np_probs = probs.detach().numpy()[0]
+            int_res = argmax_Y.numpy()[0][0]
 
-    return jsonify({'prediction': str_res, 'prob_0': str(np_probs[0]), 'probs_1': str(np_probs[-1]), 'content': content})
+            str_res = "BPNM"
+            if int_res == 1:
+                str_res = "Swimlane"
+
+            content = ""
+            if str_res == "BPNM":
+                with open(app.config['DOCUMENTATION_FILE'] + "bpnm.txt", "r") as f:
+                    content = f.read()
+            else:
+                with open(app.config['DOCUMENTATION_FILE'] + "swimlane.txt", "r") as f:
+                    content = f.read()
+
+            res = {
+                'prediction': str_res,
+                'prob_0': str(np_probs[0]),
+                'probs_1': str(np_probs[-1]),
+                'content': content,
+                'status': 'success',
+                'message': 'GNN Prediction Succesful'
+            }
+        else:
+            res = {
+                'status': 'fail',
+                'message': token_msg
+            }
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+        retries += 1
+        res["message"] = error
+        if conn:
+            conn.rollback()
+        print("Retry Attempt: " + str(retries) + " / " + str(max_retry))
+    finally:
+        if conn:
+            conn.close()
+
+    return jsonify(res)
